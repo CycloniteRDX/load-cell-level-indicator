037e714a1b27c2e134047d6a6d21c3e726a76944
CycloniteRDX <marcosbocija@gmail.com>
2026-07-22 12:16:43 +0200

    Extract application coordination module

---
+++ src/app.h
@@ -0,0 +1,15 @@
+#ifndef APP_H
+#define APP_H
+
+/*
+ * Initializes the complete application.
+ */
+void app_init(void);
+
+
+/*
+ * Executes one non-blocking application iteration.
+ */
+void app_update(void);
+
+#endif
